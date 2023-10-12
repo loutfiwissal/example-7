a3ddf23675cf4f3cd6e0188e5d1b3124bd844ade
Wissal Loutfi <147111691+loutfiwissal@users.noreply.github.com>
2023-10-12 02:32:31 +0100

    exercise-month

---
+++ exercise-month.py
@@ -0,0 +1,25 @@
+M=input("type the month name: ")
+if M=="january":
+    print("number of days is 31")
+elif M=="february":
+    print("number of days is 28")
+elif M=="march":
+    print("number of days is 31")
+elif M=="april":
+    print("number of days is 30")
+elif M=="may":
+    print("number of days is 31")
+elif M=="June":
+    print("number of days is 30")
+elif M=="july":
+  print("number of days is 31")
+elif M=="august":
+   print("number of days is 31")  
+elif M=="septembre":
+   print("number of days is 30")
+elif M=="october":
+    print("number of days is 31")
+elif M=="november":
+    print("number of days is 30")
+else :
+    print("number of days is 31")
